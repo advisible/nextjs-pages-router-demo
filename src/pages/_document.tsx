71e7f711f8ef7306d3639fd6a78bf958698d967f
adv-anton <122272843+adv-anton@users.noreply.github.com>
2024-01-24 09:53:08 +0100

    changed Script strategy to beforeInteractive to prevent execution of scripts on the server side

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
-                <Script strategy="beforeInteractive" id="adkInit">{'window.adk = window.adk || { cmd: [] };'}</Script>
-                <Script strategy="beforeInteractive" src="/main.js" />
+                <Script strategy="afterInteractive" id="adkInit">{'window.adk = window.adk || { cmd: [] };'}</Script>
+                <Script strategy="afterInteractive" src="/main.js" />
             </Head>
             <body>
                 <Main />
